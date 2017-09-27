341d5882c7af1605da52192b143157abf51915ff
maxwen <max.weninger@gmail.com>
2017-09-27 10:22:06 -0400

    base: add CPU info overlay
    Change-Id: I3c75a9c2837c9293822a33a95380cc1edb7f2b1a
    
    Signed-off-by: xyyx <xyyx@mail.ru>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -10434,6 +10434,11 @@ public final class Settings {
         public static final String RINGTONE_FOCUS_MODE = "ringtone_focus_mode";
 
         /**
+         * @hide
+         */
+        public static final String SHOW_CPU_OVERLAY = "show_cpu_overlay";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
